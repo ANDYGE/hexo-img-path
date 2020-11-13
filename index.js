197becdb66cc6a701c36980773b140ca8267ae54
geshaofei <geshaofei@126.com>
2020-11-14 06:15:26 +0800

    加入了data及data.content不为null或者undefined的判断处理

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
-var deal_image=function(data){
-    data.content= data.content.replace(/!\[(.*?)\]\((\.\.\/)+images\//,"![$1](/images/");
- 
-   }
-   hexo.extend.filter.register('before_post_render', deal_image, 9);
+var deal_image = function (data) {
+  if (data && data.content)
+    data.content = data.content.replace(
+      /!\[(.*?)\]\((\.\.\/)+images\//,
+      "![$1](/images/"
+    );
+};
+hexo.extend.filter.register("before_post_render", deal_image, 9);
